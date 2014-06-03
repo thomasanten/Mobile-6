5e50d6ad8ba28d5172f92a849603fcfc2ff870d8
Thomas Anten <thomas@tanten.nl>
2014-06-03 07:41:36 +0200

    Lekker!

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
 
 // Connectie gegevens - server26.firstfind.nl
-$con=mysqli_connect("localhost","durftean","SSander123","durftean");
+$con = mysqli_connect("localhost","durftean","SSander123","durftean");
 
 // Check connectie
 if (mysqli_connect_errno()) {
-  echo "Het is niet gelukt om verbinding op te zetten: " . mysqli_connect_error();
+	printf("Het is niet gelukt om verbinding op te zetten: ", mysqli_connect_error());
+	exit();
 }
-
-mysqli_close($con);
 
 ?>
